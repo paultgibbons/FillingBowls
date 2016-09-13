f1121bd8dc3ba3a1c3a82049d9e8081d95b85295
Paul Gibbons <paultgibbons@gmail.com>
2016-09-13 14:46:30 -0500

    fix spacing

--- src/main/java/com/paultgibbons/fillingbowls/FillingBowls.java
+++ src/main/java/com/paultgibbons/fillingbowls/FillingBowls.java
@@ -3,63 +3,63 @@ import java.util.stream.*;
 import java.util.*;
 
 public class FillingBowls {
-	public static void main(String[] args) {
-		List<Integer> l1 = IntStream.of(3, 5).boxed().collect(Collectors.toList());
-		List<Integer> l2 = IntStream.of(3, 6).boxed().collect(Collectors.toList());
-		List<Integer> l3 = IntStream.of(3, 5, 11, 17).boxed().collect(Collectors.toList());
-		System.out.println(canFill(l1, 4)); // true
-		System.out.println(canFill(l2, 4)); // false
-		System.out.println(canFill(l3, 12)); // true
-	}
-	
-	public static boolean canFill(List<Integer> capacities, int target) {
-		List<Bowl> bowls = capacities.stream().map(cap -> new Bowl(cap)).collect(Collectors.toList());
-		State initialState = new State(bowls);
-		Stack<State> stack = new Stack<>();
-		stack.push(initialState);
-		Set<State> seen = new HashSet<>();
-		seen.add(initialState);
-		while(!stack.isEmpty()) {
-			List<Bowl> current = stack.pop().bowls;
-			if (current.stream().anyMatch(bowl -> {
-					return bowl.amount == target;
-					})) {
-				return true;
-			}
-			
-			current.stream().forEach(bowl -> {
-				int orig = bowl.amount;
-				// create filling bowl states
-				if (bowl.amount < bowl.capacity) {
-					bowl.amount = bowl.capacity;
-					include(seen, stack, current);
-				}
-				// create emptying bowl states
-				if (bowl.amount > 0) {
-					bowl.amount = 0;
-					include(seen, stack, current);
-				}
-				bowl.amount = orig;
-			});
-			// for each pair a b
-			for (int i = 0; i < current.size(); i++) {
-				for (int j = 0; j < current.size(); j++) {
-					if (i != j) {
-						// create state of transferring from a to b
-						int aOrig = current.get(i).amount;
-						int bOrig = current.get(j).amount;
-						current.get(i).transferTo(current.get(j));
-						include(seen, stack, current);
-						current.get(i).amount = aOrig;
-						current.get(j).amount = bOrig;
-					}
-				}
-			}
-		}
-		return false;
-	}
-	
-	private static class Bowl {
+    public static void main(String[] args) {
+        List<Integer> l1 = IntStream.of(3, 5).boxed().collect(Collectors.toList());
+        List<Integer> l2 = IntStream.of(3, 6).boxed().collect(Collectors.toList());
+        List<Integer> l3 = IntStream.of(3, 5, 11, 17).boxed().collect(Collectors.toList());
+        System.out.println(canFill(l1, 4)); // true
+        System.out.println(canFill(l2, 4)); // false
+        System.out.println(canFill(l3, 12)); // true
+    }
+    
+    public static boolean canFill(List<Integer> capacities, int target) {
+        List<Bowl> bowls = capacities.stream().map(cap -> new Bowl(cap)).collect(Collectors.toList());
+        State initialState = new State(bowls);
+        Stack<State> stack = new Stack<>();
+        stack.push(initialState);
+        Set<State> seen = new HashSet<>();
+        seen.add(initialState);
+        while(!stack.isEmpty()) {
+            List<Bowl> current = stack.pop().bowls;
+            if (current.stream().anyMatch(bowl -> {
+                    return bowl.amount == target;
+                    })) {
+                return true;
+            }
+            
+            current.stream().forEach(bowl -> {
+                int orig = bowl.amount;
+                // create filling bowl states
+                if (bowl.amount < bowl.capacity) {
+                    bowl.amount = bowl.capacity;
+                    include(seen, stack, current);
+                }
+                // create emptying bowl states
+                if (bowl.amount > 0) {
+                    bowl.amount = 0;
+                    include(seen, stack, current);
+                }
+                bowl.amount = orig;
+            });
+            // for each pair a b
+            for (int i = 0; i < current.size(); i++) {
+                for (int j = 0; j < current.size(); j++) {
+                    if (i != j) {
+                        // create state of transferring from a to b
+                        int aOrig = current.get(i).amount;
+                        int bOrig = current.get(j).amount;
+                        current.get(i).transferTo(current.get(j));
+                        include(seen, stack, current);
+                        current.get(i).amount = aOrig;
+                        current.get(j).amount = bOrig;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private static class Bowl {
         int capacity;
         int amount;
         public Bowl(int capacity) {
@@ -129,5 +129,5 @@ public class FillingBowls {
             seen.add(s);
             stack.push(s);
         }
-    }	
+    }    
 }
